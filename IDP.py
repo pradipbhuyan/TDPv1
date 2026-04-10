2d72e89c9b56777ce2d3a4e286c648468149bdb2
pradipbhuyan <pradip.bhuyan@gmail.com>
2026-04-10 20:42:21 +0530

    Update IDP.py

--- IDP.py
+++ IDP.py
@@ -83,12 +83,15 @@ def login():
 
     with col2:
         if logo_path.exists():
-            st.image(logo_path, width=500)
-
+            st.image(logo_path, use_container_width=True)
+    
         st.markdown("### Sign In")
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
         api_key = st.text_input("OpenAI API Key", type="password")
+    
+        if st.button("Login", use_container_width=True):
+
 
         if st.button("Login", use_container_width=True):
             if username not in USERS or USERS[username]["password"] != password:
